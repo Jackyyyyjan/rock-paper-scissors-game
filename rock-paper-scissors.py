a7f93c3e8f744238ca7344d98843d9d83a9c07c0
Jackyyyyjan <kasra12namiranian@gmail.com>
2024-04-26 19:55:00 +0330

    add get_choices function

---
+++ rock-paper-scissors.py
@@ -0,0 +1,12 @@
+import random
+
+def get_choices():
+    player_choice=input("enter a choice (rock, paper, scissors): ")
+    options=["rock", "paper", "scissors"]
+    computer_choice=random.choice(options)
+    choices={"player choice" : player_choice, "computer choice" : computer_choice}
+    return choices
+
+
+a = get_choices()
+print(a)
