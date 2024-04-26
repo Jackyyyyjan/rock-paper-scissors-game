0765033f84d65458c3f33c243d18776163bf18ff
Jackyyyyjan <kasra12namiranian@gmail.com>
2024-04-26 21:49:54 +0330

    add main and complete the game!

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -31,3 +31,6 @@ def check_win(player, computer):
         else:
             return "Rock smashes scissors! You lose."
 
+choices = get_choices()
+result = check_win(choices["player"], choices["computer"])
+print(result)
