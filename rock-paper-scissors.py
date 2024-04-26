6e0d0760d606fb2d933a5e714710d0498a6e3a3d
Jackyyyyjan <kasra12namiranian@gmail.com>
2024-04-26 21:42:30 +0330

    add check_win function and modify get_choices function

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -4,7 +4,30 @@ def get_choices():
     player_choice=input("enter a choice (rock, paper, scissors): ")
     options=["rock", "paper", "scissors"]
     computer_choice=random.choice(options)
-    choices={"player choice" : player_choice, "computer choice" : computer_choice}
+    choices={"player" : player_choice, "computer" : computer_choice}
     return choices
 
+
+def check_win(player, computer):
+    print(f"You chose {player}, computer chose {computer}")
+    if player == computer:
+        return "It's a tie!"
+    
+    elif player == "rock":
+        if computer == "scissors":
+            return "Rock smashes scissors! You win!"
+        else:
+            return "Paper covers rock! You lose."
+        
+    elif player == "paper":
+        if computer == "rock":
+            return "Paper covers rock! You win!"
+        else:
+            return "Scissors cuts paper! You lose."
+        
+    elif player == "scissors":
+        if computer == "paper":
+            return "Scissors cuts paper! You win!"
+        else:
+            return "Rock smashes scissors! You lose."
 
