9001f28814087d82cc8f8541e093c1e446154fe7
Jackyyyyjan <kasra12namiranian@gmail.com>
2024-04-30 09:01:10 +0330

    add a new folder for boolean operators program

---
+++ Test/boolean_operators.py
@@ -0,0 +1,15 @@
+print(0 or 1)
+print(False or 'hey')
+print('hi' or 'hey')
+print([] or False)
+print(False or [])
+
+
+print('-----------------------------------------')
+
+
+print(0 and 1)
+print(False and 'hey')
+print('hi' and 'hey')
+print([] and False)
+print(False and [])
